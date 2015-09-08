274551666ca5d4ec3f99b636d956cf5fab074b46
Coronell <baldojapan@gmail.com>
2015-09-08 12:27:44 +0200

    Service begin

---
+++ CalDav.Host/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalDav.Host.Support;
+using Topshelf;
+
+namespace CalDav.Host
+{
+    class Program
+    {
+        private const string ServiceDescriptiveName = "Jarvis - CalDAV service";
+        private const string ServiceName = "JarvisCalDAV";
+
+        static void Main(string[] args)
+        {
+        }
+
+        private static TopshelfExitCode Start()
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.Title = "CalDAV Service";
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.Clear();
+                Banner();
+            }
+
+            return HostFactory.Run(x =>
+            {
+                x.Service<HostInstaller>(s =>
+                {
+                    s.ConstructUsing(name => new HostInstaller());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription(ServiceDescriptiveName);
+                x.SetDisplayName(ServiceDescriptiveName);
+                x.SetServiceName(ServiceName);
+            });
+        }
+
+        private static void Banner()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("DMS service - Proximo srl");
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("  install                   -> Installa il servizio");
+            Console.WriteLine("  uninstall                 -> Rimuove il servizio");
+            Console.WriteLine("  net start Intranet        -> Avvia il servizio");
+            Console.WriteLine("  net stop Intranet         -> Arresta il servizio");
+            Console.WriteLine("===================================================================");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ CalDav.Host/Support/HostInstaller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalDav.Host.Support
+{
+    public class HostInstaller
+    {
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
